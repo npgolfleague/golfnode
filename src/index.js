b032a8a168e6fc88eb79f3c7698cf7409ef20fce
ELindquist <npgolfleague@gmail.com>
2019-03-27 23:26:55 -0400

    Added Express

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import 'dotenv/config';
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
 
 console.log('Hello World');
 console.log(process.env.MY_SECRET);
+app.get('/', (req,res) => {
+    res.send('Hello again!')
+});
+
+app.listen(process.env.PORT, () =>
+    console.log('Listening on port %s', process.env.PORT),
+)
