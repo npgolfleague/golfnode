175a789507a4eb3bc9c2947cf44adfa91e2f2601
ELindquist <npgolfleague@gmail.com>
2019-03-28 21:15:42 -0400

    Add uuid

--- src/index.js
+++ src/index.js
@@ -1,15 +1,95 @@
 import 'dotenv/config';
 const express = require('express');
 const cors = require('cors');
+const uuidv4 = require('uuid/v4');
+let users = {
+    1: {
+      id: '1',
+      username: 'Robin Wieruch',
+    },
+    2: {
+      id: '2',
+      username: 'Dave Davids',
+    },
+  };
+  
+  let messages = {
+    1: {
+      id: '1',
+      text: 'Hello World',
+      userId: '1',
+    },
+    2: {
+      id: '2',
+      text: 'By World',
+      userId: '2',
+    },
+  };
 
 const app = express();
 app.use(cors());
 
 console.log('Hello World');
 console.log(process.env.MY_SECRET);
-app.get('/', (req,res) => {
-    res.send('Hello again!')
+
+app.get('/', (req, res) => {
+    return res.send('Received a GET HTTP method');
+  });
+  
+app.post('/', (req, res) => {
+return res.send('Received a POST HTTP method');
 });
+
+app.put('/', (req, res) => {
+return res.send('Received a PUT HTTP method');
+});
+
+app.delete('/', (req, res) => {
+return res.send('Received a DELETE HTTP method');
+});
+
+app.get('/users', (req, res) => {
+return res.send(Object.values(users));
+});
+
+app.get('/users/:userId', (req, res) => {
+return res.send(users[req.params.userId]);
+});  
+app.post('/users', (req, res) => {
+return res.send('POST HTTP method on user resource');
+});
+
+app.put('/users/:userId', (req, res) => {
+    return res.send(
+      `PUT HTTP method on user/${req.params.userId} resource`,
+    );
+  });
+  
+app.delete('/users/:userId', (req, res) => {
+return res.send(
+    `DELETE HTTP method on user/${req.params.userId} resource`,
+);
+});
+
+app.get('/messages', (req, res) => {
+    return res.send(Object.values(messages));
+  });
+  
+app.get('/messages/:messageId', (req, res) => {
+return res.send(messages[req.params.messageId]);
+});
+
+app.post('/messages', (req, res) => {
+    const id = uuidv4();
+    const message = {
+      id,
+      text: req.body.text,
+    };
+  
+    messages[id] = message;
+  
+    return res.send(message);
+  });
 
 app.listen(process.env.PORT, () =>
     console.log('Listening on port %s', process.env.PORT),
